ef3a1ab6fa234f9004ec5ae1f783dff7736936c5
taxio <taxio.2029@gmail.com>
2017-11-03 20:43:18 +0900

    とりあえずフィールドとボール追加

---
+++ Assets/Scripts/ballManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.gravity = new Vector3(0, -9.81f*1.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/fieldManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fieldManager : MonoBehaviour {
+
+	float x = 0;
+	float z = 0;
+	const float power = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			z -= Time.deltaTime * power;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			z += Time.deltaTime * power;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			x += Time.deltaTime * power;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			x -= Time.deltaTime * power;
+		}
+
+		transform.rotation = Quaternion.Euler (x, 0, z);
+	}
+}
